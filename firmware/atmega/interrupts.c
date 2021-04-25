a4e43c6a57675bc80e86893a0f9587adc58392b6
Jacob Killelea <jkillelea@protonmail.ch>
2021-04-25 11:04:35 -0500

    Cleanup + comments

--- firmware/atmega/interrupts.c
+++ firmware/atmega/interrupts.c
@@ -8,27 +8,40 @@
 #include "display_segments.h"
 
 // Digit cycling variable
-volatile int digit  = 0;
+volatile int digit = 0;
 
-// SPI interrupt
+/* SPI interrupt
+ * =============
+ * Shift in a new number to render
+ */
 ISR(SPI_STC_vect)
 {
     g_DisplayNumber = (g_DisplayNumber << 8) | SPDR;
 }
 
-// Timer 1 overflow
+/* Timer 1 overflow 
+ * ================
+ * Unused
+ */
 ISR(TIMER1_OVF_vect)
 {
 }
 
-// Timer 1 compare match
+/* Timer 1 compare match
+ * =====================
+ * This function updtes the rendered number
+ * on the screen by setting selecting a digit
+ * and updating the segments to light up from
+ * the table accordingly
+ */
 ISR(TIMER1_COMPA_vect)
 {
     // render a nybble
     if (digit < 4)
     {
-        GpioD->port.byte = g_DisplayRenderBuffer[digit];
+        GpioD->port.byte = 0;
         selectDigit(digit);
+        GpioD->port.byte = g_DisplayRenderBuffer[digit];
     }
     digit = (digit + 1) % 4;
 }
